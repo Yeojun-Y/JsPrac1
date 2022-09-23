0279be72e99ce6d5dfbe56ae111a5e5da4512336
Yeojun-Y <ikm3256@naver.com>
2022-09-23 17:38:22 +0900

    5

--- operator.js
+++ operator.js
@@ -94,3 +94,114 @@ for (let i = 0; i < 3; i++) {
         console.log(`i : ${i}, j : ${j}`);
     }
 }
+
+function log(message) {
+    console.log(message);
+}
+log('Hellllo');
+log(1234);
+
+console.log('@@@@@@@@@#@@@9');
+//Default Parameters
+function showMessage(message, from = 'unknown') {
+    console.log(`${message} by ${from}`);
+}
+showMessage('2');
+
+console.log('@@@@@@@@@#@@10');
+//Rest Parameters //배열형태로 전달
+function printAll(...args) {
+    for (let i = 0; i < args.length; i++) {
+        console.log(args[i]);
+    }
+}
+printAll('a', 'B', 'c');
+
+console.log('@@@@@@@@@#@@11');
+//Rest Parameters //배열형태로 전달
+function printAll(...args) {
+    for (const arg of args) {
+        console.log(arg);
+    }
+    //혹은
+    //args.forEach((arg) => console.log(arg));
+}
+printAll('ab', 'Bc', 'cD');
+
+console.log('@@@@@@@@@#@@12');
+//Local scope 
+let globalMessage = 'global';  //global variable
+function printMessage() {
+    let message = 'hello';
+    console.log(message); //local variable
+    console.log(globalMessage);
+}
+printMessage();
+console.log('@@@@@@@@@#@@13');
+//Return a value
+function sum(a, b) {
+    return a + b;
+}
+const result = sum(1, 2);
+console.log(result);
+console.log(`sum : ${sum(1, 2)}`);
+console.log(`sum : ${result}`);
+
+console.log('@@@@@@@@@#@@14');
+//Early returnm early exit
+//bad example
+function upgradeUser(user) {
+    if (user.point > 10) {
+        //long logic...
+    }
+}
+
+//good example
+function upgradeUser(user) {
+    if (user.point <= 10) {
+        return;
+    }//빨리 리턴해주고 작업하자
+    //long logic...
+}
+console.log('@@@@@@@@@#@@15');
+//callback 
+function randomQuiz(answer, printYes, printNo) {
+    if (answer === 'love you') {
+        printYes();
+    } else {
+        printNo();
+    }
+}
+const printYes = function () {
+    console.log("correct");
+}
+const printNo = function () {
+    console.log("no!!!!");
+}
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love you', printYes, printNo);
+
+console.log('@@@@@@@@@#@@16');
+//Arrow function
+//always annoymous
+const simplePrint1 = function () {
+    console.log('simplePrint!');
+}; //이런 모양을
+const simplePrint2 = () => console.log('simplePrint!!');
+
+///////////////////////////
+const add1 = function (a, b) {
+    return a + b;
+} //이런 모양을 
+const add2 = (a, b) => a + b;
+//혹은
+const add3 = (a, b) => {
+    // - - -
+    return a + b;
+}
+////////////////////////////////////////
+console.log('@@@@@@@@@#@@17');
+//IIFE: immediately Invoked Function Expression
+(function hello() {
+    console.log('IIFE');
+})();
