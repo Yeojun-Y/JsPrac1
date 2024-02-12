331b6cf9f7d47a1db665368d0458ae19ffb96759
Yeojun-Y <105261774+Yeojun-Y@users.noreply.github.com>
2024-02-12 20:43:04 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,9 @@
+function solution(my_string, num1, num2) {
+    let strArr = my_string.split('');
+    let temp = strArr[num1];
+    strArr[num1] = strArr[num2];
+    strArr[num2] = temp;
+    
+    let answer = strArr.join(''); 
+    return answer;
+}
