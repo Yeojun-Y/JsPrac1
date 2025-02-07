e638c9e86723ab4d98c66abd8ce52cb8fbd1d368
Yeojun-Y <105261774+Yeojun-Y@users.noreply.github.com>
2025-02-07 21:40:37 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.21 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/README.md
+++ 프로그래머스/0/120902. 문자열 계산하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 계산하기 - 120902 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120902) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120902?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.06 ms
+메모리: 33.3 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 07일 12:12:29
+2025년 02월 07일 21:40:34
 
 ### 문제 설명
 
--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,4 +1,30 @@
 function solution(my_string) {
     var answer = 0;
-    return answer = eval(my_string);
+    // return answer = eval(my_string); //보안에 위험하다고 함
+    
+    let currentNum = '';
+    let lastOpr = '+';
+    for(let char of my_string) {
+        if(char >= '0' && char <= '9'){
+            currentNum += char
+        } else if (['+', '-'].includes(char) || char === ''){
+            if(currentNum) {
+                if(lastOpr === '+') {
+                    answer += parseInt(currentNum);
+                } else if ( lastOpr === '-') {
+                    answer -= parseInt(currentNum);
+                }
+            }
+            currentNum = '';
+            lastOpr = char;
+        }
+    }
+    if(currentNum) {
+        if(lastOpr === '+') {
+            answer += parseInt(currentNum);
+        } else if ( lastOpr === '-') {
+            answer -= parseInt(currentNum);
+        }
+    }
+    return answer
 }
